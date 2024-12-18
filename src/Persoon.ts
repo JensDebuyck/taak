0d7ff48cef7b3c4f28370114521b6feb9847c3f0
Jens Debuyck <jens.debuyck@outlook.com>
2024-12-18 10:46:18 +0100

    Toevoegen afbeelding

---
+++ src/Persoon.ts
@@ -0,0 +1,8 @@
+export class Persoon{
+    constructor(id:number, naam:string){
+        this.id = id;
+        this.naam = naam;
+    }
+    id:number;
+    naam:string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Persoon } from '../Persoon';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'taak';
+  static readonly PERSONEN: Persoon[] = [
+    { id: 1, naam: 'Ann' },
+    { id: 2, naam: 'Elise' },
+    { id: 3, naam: 'Marie' },
+    { id: 4, naam: 'Els' },
+    { id: 5, naam: 'Jan' },
+    { id: 6, naam: 'Piet' },
+    { id: 7, naam: 'Joris' },
+    { id: 8, naam: 'Jos' },
+    { id: 9, naam: 'Ken' },
+    { id: 10, naam: 'Andrew' },
+  ];
+  title = 'Taak';
+  personen = AppComponent.PERSONEN;
 }
